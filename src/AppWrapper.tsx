f96b88ecf00acfce209f4f886a3fadaa8ad3da9a
night-fury-rider <5191208+night-fury-rider@users.noreply.github.com>
2024-11-02 08:40:27 +0530

    Integrated Theming.

--- src/AppWrapper.tsx
+++ src/AppWrapper.tsx
@@ -1,13 +1,32 @@
+import {useEffect, useState} from 'react';
+import {PaperProvider} from 'react-native-paper';
+import {ThemeProp} from 'react-native-paper/lib/typescript/types';
+import {Appearance} from 'react-native';
 import {Provider} from 'react-redux';
 
 import App from 'App';
 import {store} from '$common/redux/store';
+import LoggerService from '$common/services/LoggerService';
+import {getTheme} from '$common/services/ThemeService';
 
 const AppWrapper = () => {
+  const [colorScheme, setColorScheme] = useState(Appearance.getColorScheme());
+
+  useEffect(() => {
+    const subscription = Appearance.addChangeListener(({colorScheme}) => {
+      LoggerService.log(`System Color Scheme changed to ${colorScheme}`);
+      setColorScheme(colorScheme);
+    });
+    return () => subscription.remove();
+  }, []);
+
+  const theme = getTheme(colorScheme as ThemeProp, 0);
   return (
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <PaperProvider theme={theme}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </PaperProvider>
   );
 };
 
--- src/common/services/ThemeService.ts
+++ src/common/services/ThemeService.ts
@@ -1,6 +1,6 @@
 // This file is inteneded to contain all theming (custom fonts, coloring, dark mode, light mode etc.) related stuff in this file.
+// Theme contains colors, fonts etc.
 
-import {StyleSheet} from 'react-native';
 import {Appearance} from 'react-native';
 
 import {
@@ -10,96 +10,60 @@ import {
   ORANGE_COLOR_THEME,
 } from '$common/constants/colors.constants';
 import {COMMON} from '$common/constants/strings.constants';
+import {DefaultTheme, MD3DarkTheme, MD3LightTheme} from 'react-native-paper';
+import {ThemeProp} from 'react-native-paper/lib/typescript/types';
 
-let selectedLightColors;
-let selectedDarkColors;
-let customTextColor1;
-let customTextColor2;
-let customBackgroundColor;
+const getDefaultTheme = (themeName = COMMON.colorScheme.light as ThemeProp) => {
+  const lightTheme = {
+    ...DefaultTheme,
+    ...MD3LightTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      ...MD3LightTheme.colors,
+    },
+  };
 
-const getStyles = (
-  textColor1?: string,
-  textColor2?: string,
-  backgroundColor?: string,
-) => {
-  const customTextColor1 = textColor1 ? textColor1 : 'black';
-  const customTextColor2 = textColor2 ? textColor2 : 'black';
-  const customBackgroundColor = backgroundColor ? backgroundColor : 'white';
+  const darkTheme = {
+    ...MD3DarkTheme,
+    colors: {
+      ...MD3DarkTheme.colors,
+    },
+  };
 
-  const styles = StyleSheet.create({
-    customText1: {
-      color: customTextColor1,
-    },
-    customText2: {
-      color: customTextColor2,
-    },
-    normalFont: {
-      fontFamily: 'IBMPlexSerif',
-    },
-    boldFont: {
-      fontFamily: 'IBMPlexSerif-Bold',
-      fontWeight: '600',
-    },
-    boldItalicFont: {
-      fontFamily: 'IBMPlexSerif-BoldItalic',
-      fontWeight: '600',
-    },
-    centerAlign: {
-      textAlign: 'center',
-    },
-    background1: {
-      backgroundColor: customBackgroundColor,
-    },
-  });
-
-  return styles;
+  return themeName === COMMON.colorScheme.light ? lightTheme : darkTheme;
 };
 
-// Keep all font family info in this function only
-const getTheme = (selectedColorThemeIndex: number) => {
+const getCustomThemeColors = (
+  themeName = COMMON.colorScheme.light as ThemeProp,
+  selectedColorThemeIndex = 0,
+) => {
   switch (selectedColorThemeIndex) {
     case 1:
-      selectedLightColors = ORANGE_COLOR_THEME;
-      selectedDarkColors = DARK_ORANGE_COLOR_THEME;
-      break;
+      return themeName === COMMON.colorScheme.light
+        ? ORANGE_COLOR_THEME
+        : DARK_ORANGE_COLOR_THEME;
 
     case 0:
     default:
-      selectedLightColors = BLUE_COLOR_THEME;
-      selectedDarkColors = DARK_BLUE_COLOR_THEME;
+      return themeName === COMMON.colorScheme.light
+        ? BLUE_COLOR_THEME
+        : DARK_BLUE_COLOR_THEME;
   }
-
-  customTextColor1 = isDarkModeON()
-    ? selectedDarkColors.text1
-    : selectedLightColors.text1;
-
-  customTextColor2 = isDarkModeON()
-    ? selectedDarkColors.text2
-    : selectedLightColors.text2;
-
-  customBackgroundColor = isDarkModeON()
-    ? selectedDarkColors.background1
-    : selectedLightColors.background1;
-
-  const styles = getStyles(
-    customTextColor1,
-    customTextColor2,
-    customBackgroundColor,
-  );
-
-  return {darkColors: {}, lightColors: {}};
-};
-
-const getColors = (selectedColorThemeIndex: number) => {
-  const theme = getTheme(selectedColorThemeIndex);
-
-  if (isDarkModeON()) {
-    return theme.darkColors;
-  }
-  return theme.lightColors;
 };
 
 const isDarkModeON = () =>
   Appearance.getColorScheme() === COMMON.colorScheme.dark;
 
-export {getTheme, getColors, isDarkModeON};
+const getTheme = (
+  themeName = COMMON.colorScheme.light as ThemeProp,
+  selectedColorThemeIndex = 0,
+): ThemeProp => {
+  let customTheme = getDefaultTheme(themeName);
+  customTheme.colors = {
+    ...customTheme.colors,
+    ...getCustomThemeColors(themeName, selectedColorThemeIndex),
+  };
+  return customTheme;
+};
+
+export {getTheme, isDarkModeON};
